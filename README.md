cd5d7a857394de766e1cb796a7b115fb2bc82394
Sven Hesse <drmccoy@drmccoy.de>
2022-11-23 19:51:00 +0100

    Add empty project frame

---
+++ README.md
@@ -0,0 +1,10 @@
+caldav2pal README
+=================
+
+caldav2pal is a free/libre and open source (FLOSS) utility to convert
+[CalDAV](https://en.wikipedia.org/wiki/CalDAV)/[CardDAV](https://en.wikipedia.org/wiki/CardDAV)
+to [pal](https://palcal.sourceforge.net/) event files, licensed under the
+terms of the [GNU Affero General Public License version 3](https://www.gnu.org/licenses/agpl.html)
+(or later).
+
+You can find [caldav2pal here on GitHub](https://github.com/DrMcCoy/caldav2pal).
---
+++ src/caldav2pal.py
@@ -0,0 +1,27 @@
+"""! Main entry point for caldav2pal.
+"""
+
+# caldav2pal - A FLOSS utility to convert CalDAV/CardDAV to pal
+# Copyright (c) 2022 Sven "DrMcCoy" Hesse <drmccoy@drmccoy.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+def main() -> None:
+    """! caldav2pal main function.
+    """
+
+
+if __name__ == '__main__':
+    main()
