69711cf283e91b3495a8aedad5f99150d31e155a
Sven Hesse <drmccoy@drmccoy.de>
2022-11-26 01:01:16 +0100

    Handle command line arguments

--- src/caldav2pal.py
+++ src/caldav2pal.py
@@ -17,13 +17,42 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import argparse
+from argparse import RawTextHelpFormatter
+
 from calendars2events import convert_calendars_to_events
 from contacts2birthdays import convert_contacts_to_birthdays
+from util import Util
+
+
+def _parse_args() -> argparse.Namespace:
+    """! Parse command line arguments.
+
+    @return An object containing the parsed command line arguments.
+    """
+    info = Util.get_project_info()
+    nameversion = f"{info['name']} {info['version']}"
+    description = f"{nameversion} -- {info['summary']}"
+    version = (f"{nameversion}\n"
+               f"{info['url']['homepage']}\n\n"
+               f"Copyright (c) {info['years']} {', '.join(info['authors'])}\n\n"
+               "This is free software; see the source for copying conditions.  There is NO\n"
+               "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.")
+
+    parser = argparse.ArgumentParser(description=description, formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument("-v", "--version", action="version",
+                        version=version,
+                        help="print the version and exit")
+
+    return parser.parse_args()
 
 
 def main() -> None:
     """! caldav2pal main function.
     """
+    _parse_args()
+
     convert_contacts_to_birthdays()
     convert_calendars_to_events()
 
